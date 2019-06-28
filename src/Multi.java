0a57128ac85152a3a013638d7ce662700480a7ff
mukultarania <taraniamukul@gmail.com>
2019-06-28 22:28:37 +0530

    ver 0.1

---
+++ src/Multi.java
@@ -0,0 +1,36 @@
+class Hi extends Thread{
+    public void run(){
+        for(int i = 0; i <5; i++){
+            System.out.println("Hii");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class Hello extends Thread{
+    public void run(){
+        for(int i = 0; i <5; i++){
+            System.out.println("Hello");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class Multi {
+    public static void main(String[] a){
+        Hi hi = new Hi();
+        Hello hello = new Hello();
+
+        hi.start();
+        hello.start();
+    }
+
+}
